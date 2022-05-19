0d35cfd1a160044c0d1d5fd330d351b1ef6fd558
Dalpat Singh <dalpatsinghpurohit@gmail.com>
2022-05-19 13:55:44 +0530

    Initial Commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'e0cbdb24f07d93d980137dbc0532461c1ec91d72',
+        'name' => 'innowaysit/currency-formatter',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'innowaysit/currency-formatter' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'e0cbdb24f07d93d980137dbc0532461c1ec91d72',
+            'dev_requirement' => false,
+        ),
+    ),
+);
