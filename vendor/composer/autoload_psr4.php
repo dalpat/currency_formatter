fd4d4ddc3884e2da3d9fcb02d21f3fcba5e3aab9
Dalpat Singh <dalpatsinghpurohit@gmail.com>
2022-05-19 14:53:11 +0530

    auto gen files

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,5 +6,9 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Tests\\' => array($baseDir . '/tests'),
+    'PhpParser\\' => array($vendorDir . '/nikic/php-parser/lib/PhpParser'),
     'Innowaysit\\CurrencyFormatter\\' => array($baseDir . '/src'),
+    'Doctrine\\Instantiator\\' => array($vendorDir . '/doctrine/instantiator/src/Doctrine/Instantiator'),
+    'DeepCopy\\' => array($vendorDir . '/myclabs/deep-copy/src/DeepCopy'),
 );
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -50,6 +50,24 @@ class ComposerAutoloaderInit8a3b1a54bed37cb393a039994ec7dd8b
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit8a3b1a54bed37cb393a039994ec7dd8b::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire8a3b1a54bed37cb393a039994ec7dd8b($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire8a3b1a54bed37cb393a039994ec7dd8b($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
